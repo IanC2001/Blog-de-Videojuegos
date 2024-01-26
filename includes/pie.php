c06e409710b099b4e831d26ae5ba80c08edf0493
ian <iancane001@gmail.com>
2024-01-26 15:35:17 -0300

    first commit

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Ian Cané &copy; 2024</p>
+		</footer>
+		
+	</body>
+</html>
